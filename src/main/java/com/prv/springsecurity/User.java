1f45d60a52def3a6aaf967f5d600f58eea163d32
imadcollin <imad.collin@gmail.com>
2021-09-06 23:58:01 +0200

    Add user class

---
+++ src/main/java/com/prv/springsecurity/User.java
@@ -0,0 +1,48 @@
+package com.prv.springsecurity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    private String firstName;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String lastName;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
