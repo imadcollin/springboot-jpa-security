bfacf14065f408332dbda3e9bb6dc4b1b5d409a2
imadcollin <imad.collin@gmail.com>
2021-09-06 23:50:17 +0200

    Add context config class path

--- src/main/java/com/prv/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/prv/springsecurity/SpringSecurityApplication.java
@@ -1,7 +1,17 @@
 package com.prv.springsecurity;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@WebAppConfiguration
+
+@ContextConfiguration(locations = {"classpath:applicationContext*.xml", "classpath:spring-servlet.xml"})
 
 @SpringBootApplication
 public class SpringSecurityApplication {
