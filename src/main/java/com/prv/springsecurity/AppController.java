ccadb7d64a7071462795f7df81c5d024cc26412a
imadcollin <imad.collin@gmail.com>
2021-09-08 13:29:23 +0200

    Add listOfUser mapping and reg pass

--- src/main/java/com/prv/springsecurity/AppController.java
+++ src/main/java/com/prv/springsecurity/AppController.java
@@ -1,10 +1,13 @@
 package com.prv.springsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -23,8 +26,21 @@ public class AppController {
         return "register";
     }
 
+    @GetMapping("/list_users")
+    public String user(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("listOfUser", users);
+        return "list_users";
+    }
+
     @PostMapping("/reg")
     public String registration(User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String passCode = encoder.encode(user.getPassword());
+        user.setPassword(passCode);
+        System.out.println(user.getPassword());
+        System.out.println(passCode);
+
         userRepository.save(user);
         System.out.println(user);
         return "done";
