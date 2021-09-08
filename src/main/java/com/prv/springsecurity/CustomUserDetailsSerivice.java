549cb3432f3456a3a58da9a637e1c9b33258a4b3
imadcollin <imad.collin@gmail.com>
2021-09-08 15:16:26 +0200

    Make find by email instead of name

--- src/main/java/com/prv/springsecurity/CustomUserDetailsSerivice.java
+++ src/main/java/com/prv/springsecurity/CustomUserDetailsSerivice.java
@@ -10,8 +10,8 @@ public class CustomUserDetailsSerivice implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String firstName) throws UsernameNotFoundException {
-        User user = userRepository.findByFirstName(firstName);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
         if (user == null) {
             throw new UsernameNotFoundException("User not found... ");
         }
--- src/main/java/com/prv/springsecurity/UserRepository.java
+++ src/main/java/com/prv/springsecurity/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.firstName=?1")
-    User findByFirstName(String firstName);
+    @Query("SELECT u FROM User u WHERE u.email=?1")
+    User findByEmail(String email);
 
 }
