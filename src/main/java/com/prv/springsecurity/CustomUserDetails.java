1d22aab866d9bdfecddec45c86fb5db156fd68e7
imadcollin <imad.collin@gmail.com>
2021-09-08 17:09:00 +0200

    Log the username on singing

--- src/main/java/com/prv/springsecurity/CustomUserDetails.java
+++ src/main/java/com/prv/springsecurity/CustomUserDetails.java
@@ -47,4 +47,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public String getFirstName(){
+        return user.getFirstName()+" "+user.getLastName();
+    }
 }
